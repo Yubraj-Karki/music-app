9fee711cc592efbc30abccaafd3926ee502a9e36
YubrajKarki <yubrajkarki528@gmail.com>
2023-02-15 20:24:57 +0545

    setting up tailwind css

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+// import "./App.css";
+// import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <h1>Hellow world</h1>
+    <div className="bg-blue-500">
+      <h1 className="text-3xl font-bold underline">Hello world</h1>
     </div>
   );
 }
