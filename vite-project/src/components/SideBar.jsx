bb3f66dc832b6b4e3d241fc43ff9fc7adcb3b29c
YubrajKarki <yubrajkarki528@gmail.com>
2023-02-16 22:51:23 +0545

    sidebar setup

--- vite-project/src/components/SideBar.jsx
+++ vite-project/src/components/SideBar.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { RiCompassDiscoverLine } from "react-icons/ri";
+import { GiMicrophone } from "react-icons/gi";
+import { IoAlbumsOutline } from "react-icons/io5";
 
 const SideBar = () => {
-  return <div>SideBar</div>;
+  return (
+    <div className="side-bar p-10 h-screen w-60 text-white">
+      <h3 className="font-bold text-3xl">Music App</h3>
+      <ul className="menus">
+        <li>
+          <RiCompassDiscoverLine />
+          Explore
+        </li>
+        <li>
+          <GiMicrophone />
+          Artists
+        </li>
+        <li>
+          <IoAlbumsOutline /> Albums
+        </li>
+        <li>Playlist</li>
+        <li>Liked songs</li>
+      </ul>
+    </div>
+  );
 };
 
 export default SideBar;
+<ul></ul>;
