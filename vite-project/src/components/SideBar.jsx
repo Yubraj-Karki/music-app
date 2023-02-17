b9669c21067130ec4a570b4d900d8ab1da98d0ac
YubrajKarki <yubrajkarki528@gmail.com>
2023-02-17 14:05:22 +0545

    adding link to the menus

--- vite-project/src/components/SideBar.jsx
+++ vite-project/src/components/SideBar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { RiCompassDiscoverLine } from "react-icons/ri";
 import { GiMicrophone } from "react-icons/gi";
 import { IoAlbumsOutline } from "react-icons/io5";
@@ -10,23 +11,33 @@ const SideBar = () => {
     <div className="side-bar p-10 h-screen w-64 text-white">
       <h3 className="font-bold text-3xl">Music App</h3>
       <ul className="menus">
-        <li>
-          <RiCompassDiscoverLine />
-          Explore
-        </li>
-        <li>
-          <GiMicrophone />
-          Artists
-        </li>
-        <li>
-          <IoAlbumsOutline /> Albums
-        </li>
-        <li>
-          <TbPlaylist /> Playlist
-        </li>
-        <li>
-          <AiFillHeart /> Liked songs
-        </li>
+        <Link to="/">
+          <li>
+            <RiCompassDiscoverLine />
+            Explore
+          </li>
+        </Link>
+        <Link to="/artists">
+          <li>
+            <GiMicrophone />
+            Artists
+          </li>
+        </Link>
+        <Link to="/albums">
+          <li>
+            <IoAlbumsOutline /> Albums
+          </li>
+        </Link>
+        <Link to="/playlist">
+          <li>
+            <TbPlaylist /> Playlist
+          </li>
+        </Link>
+        <Link to="/likedSongs">
+          <li>
+            <AiFillHeart /> Liked songs
+          </li>
+        </Link>
       </ul>
     </div>
   );
