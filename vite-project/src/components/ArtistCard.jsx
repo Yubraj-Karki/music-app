9e7c815249efbde801d00f1b810f24026552668d
YubrajKarki <yubrajkarki528@gmail.com>
2023-02-19 17:34:00 +0545

    card structure

--- vite-project/src/components/ArtistCard.jsx
+++ vite-project/src/components/ArtistCard.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const ArtistCard = () => {
-  return <div></div>;
+  return (
+    <div className="artist-card">
+      <div className="artist-card-img">
+        <img src="" alt="" />
+      </div>
+      <h3>Whole lotta love</h3>
+      <param>Led Zeppelin</param>
+    </div>
+  );
 };
 
 export default ArtistCard;
