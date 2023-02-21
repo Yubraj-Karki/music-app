898ab8f0c3258fb05224e22dda9f1c71a49dbef0
YubrajKarki <yubrajkarki528@gmail.com>
2023-02-21 15:31:39 +0545

    cleaning up

--- vite-project/src/components/Searchbar.jsx
+++ vite-project/src/components/Searchbar.jsx
@@ -4,7 +4,7 @@ const Searchbar = () => {
   return (
     <div>
       <input
-        className="rounded-full  bg-[#0C0B39] outline-0 p-2.5 pl-10 w-1/2"
+        className="rounded-full text-[11px] bg-[#121059] outline-0 p-[18px] pl-[30px] w-[37%]"
         type="text"
         placeholder="Search music, artists, and albums"
       ></input>
--- vite-project/src/components/SideBar.jsx
+++ vite-project/src/components/SideBar.jsx
@@ -5,11 +5,12 @@ import { GiMicrophone } from "react-icons/gi";
 import { IoAlbumsOutline } from "react-icons/io5";
 import { AiFillHeart } from "react-icons/ai";
 import { TbPlaylist } from "react-icons/tb";
+import { MdHeadphones, MdOutlineTravelExplore } from "react-icons/md";
 
 const SideBar = () => {
   return (
-    <div className="side-bar bg-[#0C0B39] p-10 h-screen w-[273px] text-white">
-      <div className="logo-title flex items-center">
+    <div className="side-bar bg-[#0C0B39] p-10 pt-[20px] h-screen w-[273px] text-white">
+      <div className="logo-title flex items-center mb-[46px]">
         <img
           className="h-[39px] w-[39px] mr-[10px]"
           src="/logo.png"
@@ -21,13 +22,13 @@ const SideBar = () => {
       <ul className="menus text-[16px]">
         <Link to="/">
           <li>
-            <RiCompassDiscoverLine />
+            <MdOutlineTravelExplore />
             Explore
           </li>
         </Link>
         <Link to="/artists">
           <li>
-            <GiMicrophone />
+            <MdHeadphones />
             Artists
           </li>
         </Link>
