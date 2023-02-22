00f4ec04b7615d0bac90479b80404fbc68b227d5
YubrajKarki <yubrajkarki528@gmail.com>
2023-02-22 23:59:14 +0545

    made control fixed to the bottom

--- vite-project/src/components/Control.jsx
+++ vite-project/src/components/Control.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Control = () => {
-  return <div>Control</div>;
+  return <div className="fixed bottom-0 left-0 right-0">Control</div>;
 };
 
 export default Control;
