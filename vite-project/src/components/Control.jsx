dfe9355ed19303d0299fdae476f11317d8270d65
YubrajKarki <yubrajkarki528@gmail.com>
2023-02-24 13:09:35 +0545

    volume ui and others

--- vite-project/src/components/Control.jsx
+++ vite-project/src/components/Control.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { AiOutlineHeart } from "react-icons/ai";
 import { ImPause2, ImPlay3, ImShuffle, ImLoop } from "react-icons/im";
 import { RxTrackNext, RxTrackPrevious } from "react-icons/rx";
+import { CiPause1, CiVolumeHigh, CiVolumeMute } from "react-icons/ci";
+import { CgPlayListAdd } from "react-icons/cg";
 
 const Control = () => {
   return (
@@ -24,18 +26,29 @@ const Control = () => {
           <div className="text-[21px] font-normal text-[#BABABA]">
             <AiOutlineHeart />
           </div>
-          <p className="ml-[15%]">4:30-5:36</p>
+          <p className="ml-[15%] text-[12px] text-[#BABABA]">4:30-5:36</p>
         </div>
-        <div className="flex items-center justify-between w-[75%] text-[25px]">
-          <ImLoop />
-          <RxTrackPrevious />
-          <span className="flex items-center h-[40px] w-[40px] bg-[#fff] text-[#000] rounded-full">
-            <ImPause2 />
+        <div className="flex justify-items-center items-center justify-between w-[60%] text-[25px]">
+          <ImLoop className="text-[20px]" />
+          <RxTrackPrevious className="ml-[20px]" />
+          <span className="flex justify-center items-center h-[40px] w-[40px] bg-[#fff] text-black rounded-full">
+            <CiPause1 />
           </span>
-          <RxTrackNext />
-          <ImShuffle />
+          <RxTrackNext className="mr-[20px]" />
+          <ImShuffle className="text-[20px]" />
         </div>
-        <div>volume</div>
+        <div className="text-[20px] justify-self-end">
+          <div className="flex items-center">
+            <CgPlayListAdd />
+
+            <CiVolumeHigh className="ml-[15px]" />
+            <div className="volume w-[100px] h-[3px] bg-[#D9D9D9] ml-[10px]">
+              <div className="relative volume w-[34px] h-[3px] bg-[#F5E33F]">
+                <span className="absolute right-0 top-[-2px] rounded-full w-[9px] h-[9px] bg-[#F5E33F]"></span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
