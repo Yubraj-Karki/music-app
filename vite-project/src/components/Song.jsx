82291d14b33bdb0a85b2faa985ea644f29980f67
Yubraj-Karki <yubrajkarki528@gmail.com>
2023-05-08 21:03:56 +0545

    fixed the layout use

--- vite-project/src/components/Song.jsx
+++ vite-project/src/components/Song.jsx
@@ -47,39 +47,40 @@ const Song = ({ id, name, artist, album, duration, img, index }) => {
 
 
   return (
-    <div className="group grid grid-cols-7 gap-2 mx-auto mb-[25px] p-[9px] hover:bg-[#19177F] w-full">
-      <p className="col-span-0.3">{index+1}</p>
-      <div className="hidden col-start-1.2 col-span-1 md:block relative cursor-pointer song-img-container max-h-[47.33px] my-[0px] mx-[0px] w-[56.52px] overflow-hidden">
-        <FaPlay  onClick={() => {
-          handleSongPlay(id);
-        }} className="absolute opacity-0 text-white text-[20px] top-[30%] left-[30%] group-hover:opacity-100 z-20" />
+ 
+    <div className="group flex flex-row items-center justify-between gap-2 text-left mx-auto mb-[25px] px-[9px] py-[5px]  hover:bg-[#19177F] w-full">
+    <p className="">{index+1}</p>
+    <div className="hidden md:block relative cursor-pointer song-img-container h-[47.33px] my-[0px] mx-[0px] w-[56.52px] overflow-hidden">
+      <FaPlay  onClick={() => {
+        handleSongPlay(id);
+      }} className="absolute opacity-0 text-white text-[20px] top-[30%] left-[30%] group-hover:opacity-100 z-20" />
 
-        <img
-          className="object-cover z-10 group-hover:opacity-60"
-          src={img}
-          alt=""
-        />
-      </div>
-      <div className="song-title">
-        <h3 className="text-[13px] font-bold">{name}</h3>
-        <p className="text-[10px] font-normal text-[#BABABA]">{artist}</p>
-      </div>
-      <h3 className="album hidden md:block  text-[12px] font-normal text-[#BABABA]">
-        {album}
-      </h3>
-      <p className="song-length text-[12px] font-normal text-[#BABABA]">5:36</p>
-      <span
-        onClick={() => {
-          handleLike(id);
-        }}
-        className="song-heart-icon text-[18px] font-normal text-[#BABABA] cursor-pointer"
-      >
-        <AiOutlineHeart />
-      </span>
-      <span className="song-more-icon text-[20px] font-normal text-[#BABABA]">
-        <FiMoreHorizontal />
-      </span>
+      <img
+        className="object-cover z-10 group-hover:opacity-60"
+        src={img}
+        alt=""
+      />
     </div>
+    <div className="song-title w-[150px]">
+      <h3 className="text-[13px] font-bold">{name}</h3>
+      <p className="text-[10px] font-normal text-[#BABABA]">{artist}</p>
+    </div>
+    <h3 className="album w-[100px] hidden md:block  text-[12px] font-normal text-[#BABABA]">
+      {album}
+    </h3>
+    <p className="song-length text-[12px] font-normal text-[#BABABA]">5:36</p>
+    <span
+      onClick={() => {
+        handleLike(id);
+      }}
+      className="song-heart-icon text-[18px] font-normal text-[#BABABA] cursor-pointer"
+    >
+      <AiOutlineHeart />
+    </span>
+    <span className="song-more-icon text-[20px] font-normal text-[#BABABA]">
+      <FiMoreHorizontal />
+    </span>
+  </div>
   );
 };
 export default Song;
